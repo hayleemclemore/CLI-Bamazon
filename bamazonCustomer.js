070bff5d4e3bfb000320c716bd1b7ce6bf219fa7
Haylee McLemore <hmmclemore@gmail.com>
2020-04-21 13:40:25 -0500

    updating javascript file to display table to user

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,9 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table3");
 var prompt = require("prompt");
 
+//connection to mysql database
 var connection = mysql.createConnection({
     host: "localhost",
   
@@ -18,6 +20,28 @@ var connection = mysql.createConnection({
   
   connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    // querySongs();
+    console.log(`connected as id ${connection.threadId}`);
+    
+    displayProducts();
   });
+
+  function displayProducts(){
+    var query = "SELECT * FROM products"
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        var greeting = "\nHere is the current available inventory:"
+        var table = new Table({
+          head: ['ID', 'Product', 'Department', 'Price', 'In Stock'],
+          colWidths: [10, 30, 15, 10, 10, 15]
+        });
+
+        for (var i = 0; i < res.length; i++) {
+          table.push([res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price, res[i].stock_quantity]);
+        }
+        console.log(greeting);
+        console.log(table.toString());
+    });
+      connection.end();
+  }
+
+ 
